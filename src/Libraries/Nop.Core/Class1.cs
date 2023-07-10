39a15a39c2aa7a1894686e9e5703fd582c121cc7
unknown <tarequl.islam@brainstation-23.com>
2023-07-10 12:20:25 +0600

    initial commit!

---
+++ src/Libraries/Nop.Core/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core
+{
+    public static class Class1
+    {
+        public static bool IsTarequl(this string name)
+        {
+            return name == "Tarequl";
+        }
+    }
+}
